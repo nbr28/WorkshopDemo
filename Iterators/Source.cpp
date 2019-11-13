032517001f6cc872aff6dfab0d814fbed31f7e28
Natan Ross <natanross@hotmail.com>
2019-11-13 13:26:23 -0500

    Iterstor

---
+++ Iterators/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+template<class T>
+void printMe(T dataArray)
+{
+	for (auto i = dataArray.begin(); i != dataArray.end(); i++)
+	{
+		cout << *i << " ";
+	}
+	cout << endl;
+}
+
+//template<class T>
+//void printMeToo(T dataArray)
+//{
+//	for (int i=0;i<10;i++)
+//	{
+//		cout << dataArray[i] << " ";
+//	}
+//	cout << endl;
+//}
+int main()
+{
+	vector<int> vec;
+	list<int> lst;
+
+	for (int i = 0; i < 10; i++)
+	{
+		vec.push_back(i*2);
+		lst.push_back(i*2);
+	}
+
+	printMe(vec);
+	printMe(lst);
+	//printMeToo(lst);
+
+
+	int countOut = count(lst.begin(), lst.end(), 4);
+	cout << countOut << endl;
+
+	for (auto i : vec)
+	{
+		cout << i << endl;
+	}
+	return 0;
+}
